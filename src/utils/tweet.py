cd0e884efcf02af79ff3d5823011d1fd7e262704
Altynbek Orumbayev <aorumbayev@protonmail.com>
2022-10-14 01:17:04 +0200

    Removing redundant code

--- src/utils/tweet.py
+++ src/utils/tweet.py
@@ -1,12 +1,5 @@
 import tweepy
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
-
-# Create API object
-api = tweepy.API(auth)
-
 
 def get_tweepy(
     bearer_token: str,
