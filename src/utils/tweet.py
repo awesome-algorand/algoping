f9734ddf9196f32bba7df5aa1c18650a133e3e34
Altynbek Orumbayev <aorumbayev@protonmail.com>
2022-10-14 00:52:39 +0200

    Initial implementation

---
+++ src/utils/tweet.py
@@ -0,0 +1,26 @@
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+# Create API object
+api = tweepy.API(auth)
+
+
+def get_tweepy(
+    bearer_token: str,
+    consumer_key: str,
+    consumer_secret: str,
+    access_token: str,
+    access_token_secret: str,
+):
+
+    # Create API object
+    return tweepy.Client(
+        bearer_token=bearer_token,
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
+    )
